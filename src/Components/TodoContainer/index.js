9a8251f2b38541641515c166e7122e3ef59b027d
Giancarlo Gamban <gian.gamban@essilor.com>
2020-08-28 19:10:11 +0800

    Add complete todocontainer

--- src/Components/TodoContainer/index.js
+++ src/Components/TodoContainer/index.js
@@ -7,6 +7,7 @@ import "./todoContainer.css";
 class TodoContainer extends React.Component {
   state = {
     header: "",
+    todoList: [],
   };
 
   componentDidMount() {
@@ -15,39 +16,53 @@ class TodoContainer extends React.Component {
     });
   }
 
-  // addTodoItem = (todoItem) => {
-  //   const { todoList } = this.state;
-  //   const currentId = todoList[todoList.length - 1]?.id;
-  //   const todoId = (currentId ?? 0) + 1;
-  //   this.setState({
-  //     todoList: [
-  //       ...todoList,
-  //       { id: todoId, todoItem: todoItem, status: "todo" },
-  //     ],
-  //   });
-  // };
+  componentDidUpdate(prevProps, prevState){
+    const currentTodoItemCount = this.state.todoList.length;
+    if(prevState.todoList.length !== currentTodoItemCount){
+      this.setState({
+        header: `Total todoitems: ${currentTodoItemCount}`
+      })
+    }
+  }
 
-  // updateTodoItem = (todoId, status) => {
-  //   this.setState({
-  //     todoList: this.state.todoList.map((todo) =>
-  //       todo.id === todoId ? { ...todo, status: status } : todo
-  //     ),
-  //   });
-  // };
+  addTodoItem = (todoItem) => {
+    const { todoList } = this.state;
+    const currentId = todoList[todoList.length - 1]?.id;
+    const todoId = (currentId ?? 0) + 1;
+    this.setState({
+      todoList: [
+        ...todoList,
+        { id: todoId, todoItem: todoItem, status: "todo" },
+      ],
+    });
+  };
 
-  // completeTodoItem = (todoId) => {
-  //   this.updateTodoItem(todoId, "completed");
-  // };
+  updateTodoItem = (todoId, status) => {
+    this.setState({
+      todoList: this.state.todoList.map((todo) =>
+        todo.id === todoId ? { ...todo, status: status } : todo
+      ),
+    });
+  };
 
-  // cancelTodoItem = (todoId) => {
-  //   this.updateTodoItem(todoId, "canceled");
-  // };
+  completeTodoItem = (todoId) => {
+    this.updateTodoItem(todoId, "completed");
+  };
 
+  cancelTodoItem = (todoId) => {
+    this.updateTodoItem(todoId, "canceled");
+  };
 
   render() {
     return (
       <div id="todo-container">
         <h1>{this.state.header}</h1>
+        <TodoItemInput addTodoItemFunction={this.addTodoItem} />
+        <TodoList
+          todoItems={this.state.todoList}
+          cancelTodoFunction={this.cancelTodoItem}
+          completeTodoFunction={this.completeTodoItem}
+        />
       </div>
     );
   }
